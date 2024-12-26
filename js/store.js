b2603136d02a69f77de32b151ae1b2f1a658887d
Yaroslav <47635043+LischenkoYaroslav@users.noreply.github.com>
2024-12-26 19:13:19 +0000

    More prep

---
+++ js/store.js
@@ -0,0 +1,28 @@
+//Тут буде скрипт для карток продуктів
+
+console.log("Hello");
+
+// async function fetchJSON() {
+//   try {
+//     const response = await fetch("./json/store-items.json"); // Шлях до JSON
+//     console.log(response);
+//     const data = await response.json(); // Парсинг JSON
+//     console.log(data);
+//     renderData(data);
+//   } catch (error) {
+//     console.error("Error fetching JSON:", error);
+//   }
+// }
+
+// function renderData(data) {
+//   const outputDiv = document.getElementById("output");
+//   outputDiv.innerHTML = `
+//     <p>Name: ${data.name}</p>
+//     <p>Age: ${data.age}</p>
+//     <p>Is Admin: ${data.isAdmin}</p>
+//   `;
+// }
+
+// fetchJSON();
+
+console.log("World");
